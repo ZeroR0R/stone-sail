11d11c9d69178b251e14d4f44a661a9fdb546340
ZeroR0R <59980861+ZeroR0R@users.noreply.github.com>
2021-05-12 19:57:30 -0400

    Create investing.sol

---
+++ contracts/investing.sol
@@ -0,0 +1,27 @@
+pragma solidity ^0.8.0;
+
+contract Investing {
+    
+    // Deposit to a pool 
+    function deposit_eth() {}
+    function deposit_stn() {}
+    function depost_dai() {}
+    
+    // Withdraw from a pool 
+    function withdraw_eth() {}
+    function withdraw_stn() {}
+    function withdraw_dai() {}
+    
+    // Create an investing pool
+    function create_pool() {} // possibly add a delete pool to optimize gas
+    
+    // Create and modify investing strategies
+    function create_strategy() {}
+    function update_strategy() {}
+    
+    // QOl functions
+    function total_lenders() {}
+    function total_assets() {}
+    function total_gain() {}
+    function individual_report() {}
+}
