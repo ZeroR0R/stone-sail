9bebfe5d8f62b80c6de14d995350f0e9e04a4726
ZeroR0R <59980861+ZeroR0R@users.noreply.github.com>
2021-05-12 19:57:49 -0400

    Update Token.sol

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,1 +1,15 @@
 pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Main is ERC20 {
+    
+    uint256 initial_supply = 10000;
+    address owner;
+    
+    constructor() ERC20("Stones", "STN") {
+        owner = msg.sender;
+        _mint(owner, initial_supply);
+    }
+    
+}
