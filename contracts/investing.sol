c1d67840b0c210cf010b385f75d1304d8241c9db
ZeroR0R <59980861+ZeroR0R@users.noreply.github.com>
2021-05-14 18:50:12 -0400

    Update investing.sol

--- contracts/investing.sol
+++ contracts/investing.sol
@@ -5,6 +5,9 @@ import "./Interfaces.sol";
 contract Investing is InvestingAPI {
     
     address owner = msg.sender;
+    address DAI = address(0x6B175474E89094C44Da98b954EedeAC495271d0F);
+    
+    DaiToken daiToken;
     
     struct SupportedTokens {
         uint id;
@@ -43,6 +46,10 @@ contract Investing is InvestingAPI {
         
     }
     
+    constructor() {
+        daiToken = DaiToken(DAI);
+    }
+    
     function create_pool(uint _tokenID, uint256 _startingAmount, uint256 _maxAmount, string memory _name)
     external onlySupportedTokens(_tokenID) returns (bool) {
         
@@ -76,9 +83,28 @@ contract Investing is InvestingAPI {
     }
     
     function depost_dai(uint256 _amount, address _poolAddress) external returns (uint256) {
+        require(_amount >= 1, 'Your deposit must be at least 1 DAI');
+        require(pool[_poolAddress].valid == true, 'This pool is not valid');
+        require(pool[_poolAddress].initalToken == 2, 'This pool does not accept DAI');  
      
+        if (daiToken.transferFrom(msg.sender, address(this), _amount)) {
+            
+        } else {require( 1 == 2, "DAI Transfer Failed");}
+        
+        pool[_poolAddress].amount = pool[_poolAddress].amount + _amount;
+        lender[msg.sender] = Lender(_poolAddress, lender[msg.sender].amount + _amount, 0);
+    }
+    
+    function withdraw_eth(uint256 _amount) external override returns (uint256) {
+     require(_amount > 0, "You can't withdraw 0 ether");
+     require(pool[lender[msg.sender].pool].valid == true, 'This pool is not valid');
+     require(lender[msg.sender].tokenType == 0, 'This pool does not use Ether');
+     require(lender[msg.sender].amount >= _amount, 'You do not have that much invested');
      
+     address(this).transfer(msg.sender, _amount);
         
     }
+    function withdraw_stn(uint256 _amount) external returns (uint256);
+    function withdraw_dai(uint256 _amount) external returns (uint256);
     
 }
