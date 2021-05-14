dc45fe39e2a4c122c8fee9c4747ed40689b3a553
ZeroR0R <59980861+ZeroR0R@users.noreply.github.com>
2021-05-13 21:20:35 -0400

    Update investing.sol
    Added the create pool function as well as a few modifiers. In progress, currently will not compile due to not all the functions being implemented from the interface.

--- contracts/investing.sol
+++ contracts/investing.sol
@@ -1,54 +1,55 @@
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";    
+import "./Interfaces.sol";
 
-interface InvestingAPI is IERC20 {
+contract Investing is InvestingAPI {
     
-    // Deposit to a pool, returns amount deposited  
-    function deposit_eth() external returns (uint256);
-    function deposit_stn() external returns (uint256);
-    function depost_dai() external returns (uint256);
+    address owner = msg.sender;
     
-    // Withdraw from a pool, returns amount withdrawn 
-    function withdraw_eth() external returns (uint256);
-    function withdraw_stn() external returns (uint256);
-    function withdraw_dai() external returns (uint256);
+    struct SupportedTokens {
+        string name;
+        bool supported;
+    }
     
-    // Create an investing pool, returns a bool depending on success 
-    function create_pool() external returns (bool); // possibly add a delete pool to optimize gas
+    struct Pool {
+        string name;
+        string initalToken;
+        uint amount;
+        uint totalStrategies;
+        bool valid;
+    }
     
-    // Create and modify investing strategies, returns a bool depending on success 
-    function create_strategy() external returns (bool);
-    function update_strategy() external returns (bool);
-    
-    // QOl functions
-    function total_lenders() external returns (uint256);
-    function total_assets() external returns (uint256);
-    function total_gain() external returns (uint256);
-    function individual_report() external returns (uint256, uint256, uint256); // Returns Amount deposited, ammount currently, and total gain
-    function name() external view returns (string calldata);
-    function symbol() external view returns (string calldata);
-    
-}
+    mapping(string => SupportedTokens) public supportedTokens;
+    mapping(address => Pool) public pool;
 
-// Interface used to create and operate strategies
+    modifier onlyOwner() {
+        
+        require(msg.sender == owner, "Only the owner can call this function");
+        _;
+        
+    }
+    
+    modifier onlySupportedTokens(string memory tokenName_) {
+        
+        require(supportedTokens[tokenName_].supported == true, "Sorry this token is not supported.");
+        _;
+        
+    }
+    
+    function create_pool(string memory _tokenName, uint256 _startingAmount, uint256 _maxAmount, string memory _name)
+    external onlySupportedTokens(_tokenName) returns (bool) {
+        
+        pool[msg.sender] = Pool(_name, _tokenName, _startingAmount, 0, true);
+        
+        emit PoolCreated(_name, _tokenName, _startingAmount, _maxAmount);
+        
+        return true;
+    }
 
-interface StrategyAPI {
-    
-    function name() external view returns (string memory);
-    function pool() external view returns (address);
-    function valid() external view returns (bool);
-    function manager() external view returns (address); // Who controlls the strategy
-    
-    function controlledAssets() external view returns (uint256); // Assets that the strategy has 
-    function wantedAssets() external view returns (uint256); // Assets that the strategy still wants 
-    
-    function sellAsset() external; // Sells Asset to DAI 
-    function tradeAsset() external; // Trades one token for Another
-    
-    function sellWarning() external view returns (bool); // If an Asset hits too low or High 
-    
-    event SoldAsset(string Asset, uint amountSold, uint amountReceived);
-    event TradedAsset(string AssetTraded, uint amount, string AssetTradedFor, uint amountReceived);
+    function deposit_eth(uint256 _amount, address _poolAddress) external override returns (uint256) {
+        transfer()
+    }
+    function deposit_stn(uint256 _amount) external returns (uint256);
+    function depost_dai(uint256 _amount) external returns (uint256);
     
 }
